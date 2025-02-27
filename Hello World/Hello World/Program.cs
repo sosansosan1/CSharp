fda09b35d116488c74544d13efdbf7dc7f5e8b29
DESKTOP-5V3FRMH\M <tjdeh100@gmail.com>
2025-02-27 14:53:20 +0900

    어제

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void MainTitle()
+    {
+        Console.Title = "MainTitle";
+        Console.WriteLine("사람 죽이기");
+        Console.WriteLine("사람 죽이기에 오신것을 환영합니다!");
+        Console.WriteLine("저와함께 힘을 합쳐 사람들을 죽여보아요ㅎㅎ!");
+    }
+}
